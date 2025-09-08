bfbf2b089219e83185e4ea6afad9ff705e765717
anil lohar <ashwin14lohar@gmail.com>
2025-09-08 17:16:49 +0530

    Learn about nasted data tetching in react and promise.all method

---
+++ 19_Nasted_Data_Fetching/components/CountriesList.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import CountryCard from "./CountryCard";
+export default function CountriesList({ query }) {
+  const [Countriesdata, setCountriesdata] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital")
+      .then((res) => res.json())
+      .then((data) => {
+        setCountriesdata(data);
+      });
+  }, []);
+  return (
+    <>
+      <div className="country-contianer">
+        {Countriesdata.filter((country) =>
+          country.name.common.toLowerCase().includes(query)
+        ).map((country) => {
+          return (
+            <CountryCard
+              key={country.name.common}
+              href={`${country.name.common}`} 
+              flag={country.flags.svg}
+              name={country.name.common}
+              population={country.population.toLocaleString("en-IN")}
+              region={country.region}
+              capital={country.capital}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+}
